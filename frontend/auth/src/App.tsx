9b1023c4d7d87e344da4fed30c22f10e4477e1f0
Vanya Chernov <vanyachernov2001@gmail.com>
2024-09-18 20:15:20 +0300

    Add Tailwindcss & ChakraUI

--- frontend/auth/src/App.tsx
+++ frontend/auth/src/App.tsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <></>
+    <div className="bg-amber-600">
+      <h1 className="mt-2">
+        Hello, World
+      </h1>
+    </div>
   )
 }
 
